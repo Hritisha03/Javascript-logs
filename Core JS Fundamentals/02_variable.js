8fc4844cdb5f5d7dc7c76f16a6ccb661f1c0ba86
Hritisha <choudhuryhritisha03@gmail.com>
2025-06-13 11:51:50 +0530

    Basics of Javascript

---
+++ Core JS Fundamentals/02_variable.js
@@ -0,0 +1,14 @@
+var message;
+message = "Hello";
+message = "World"; // variables get updated so it will run the updated value
+console.log(message);
+
+let text; //creating variable or declaring a vriable
+text="Javascript"; //Assigning a value to a variable
+console.log(text);
+
+let myname ="Hritisha";
+console.log(myname);
+
+const number = 3;
+console.log(number);
---
+++ Core JS Fundamentals/03_naming_convention.js
@@ -0,0 +1,10 @@
+
+let userName;
+let userAge;
+let userAddress;
+
+let age;
+let _;
+let $;
+let num1;
+let num_2;
